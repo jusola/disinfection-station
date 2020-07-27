45fa8c58afbbec0cfc02857226dbad067aa114fb
jusola <juulain@gmail.com>
2020-07-27 12:51:15 +0300

    add python http

--- dispenser/net.py
+++ dispenser/net.py
@@ -8,14 +8,21 @@ transferToken = config['net']['transferToken']
 
 def sendDispensed(userid):
     # print('sending net dispensed')
+    finalAddress = urllib.parse.urljoin(address, 'comm/addScore')
+    print(finalAddress)
     return
 
 def sendNoFace():
     # here we should make a https request to nodejs server
-    finalAddress = urllib.parse.urljoin(address, 'comm', 'setFace')
+    finalAddress = urllib.parse.urljoin(address, 'comm/setFace')
     print(finalAddress)
     return
 
 def sendFace(userid):
     # here we should make a https request to nodejs server
+    finalAddress = urllib.parse.urljoin(address, 'comm/setFace')
+    print(finalAddress)
     return
+
+def post(address, body):
+    
