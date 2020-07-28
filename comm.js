ea60767ac3d3bd9790234083465950539c17d269
jusola <juulain@gmail.com>
2020-07-28 14:46:36 +0300

    Initial commit for server branch

---
+++ comm.js
@@ -0,0 +1,61 @@
+import express from 'express'
+import db from './db.js'
+const router = express.Router()
+
+class InternalCommunication{
+    constructor(){
+        this.currentUser = null
+        this.router = router
+    }
+
+    getCurrentUser = () => {
+        return this.currentUser
+    }
+
+    setCurrentUser = (newUser) => {
+        this.currentUser = newUser
+    }
+}
+
+const comm = new InternalCommunication
+
+router.use(express.json())
+router.use(express.urlencoded({ extended: true }))
+
+router.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Headers', 'Content-type,Authorization')
+    res.setHeader('Access-Control-Allow-Origin', '*')
+
+    next()
+});
+
+router.post('/setFace', async (req, res)=>{
+    try {
+        if(req.body.transferToken === process.env.transferToken){
+            comm.setCurrentUser(req.body.userid)
+            res.sendStatus(200)
+        }else{
+            res.sendStatus(403)
+        }  
+    } catch (error) {
+        res.sendStatus(500)
+    }
+
+})
+
+router.post('/addScore', async (req, res)=>{
+    try {
+        if(req.body.transferToken === process.env.transferToken){
+            await db.addScore(req.body.userid)
+            res.sendStatus(200)
+        }else{
+            res.sendStatus(403)
+        }
+    } catch (error) {
+        res.sendStatus(500)
+    }
+})
+
+
+
+export default comm
---
+++ index.js
@@ -0,0 +1,16 @@
+import 'dotenv/config.js'
+import express from 'express'
+
+import api from './api.js'
+
+import comm from './comm.js'
+
+const app = express()
+
+
+
+app.use('/api', api)
+app.use('/comm', comm.router)
+
+
+app.listen(process.env.PORT)
