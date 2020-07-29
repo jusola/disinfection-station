8c5baa9b24caf254338246ccf69f0fbed6c1b179
jusola <juulain@gmail.com>
2020-07-29 18:53:47 +0300

    Merge dispenser code from raspi and detection code

--- dispenser.py
+++ dispenser.py
@@ -1,11 +1,29 @@
 import serial
 import threading
-
+import time
+from drv8835 import DRV8835
+import RPi.GPIO as GPIO
 from config import config
+
+# GPIO tila (BOARD / BCM)
+GPIO.setmode(GPIO.BCM)
+
+# GPIO kaytettavat  pinnit
+GPIO_TRIGGER = 20
+GPIO_ECHO = 26
+car = DRV8835() # if Pololulu Motoredriver is used
+car.stop()
+# GPIO-Pinnien toimintasuunta, asettaminen (IN / OUT)
+GPIO.setup(GPIO_TRIGGER, GPIO.OUT)
+GPIO.setup(GPIO_ECHO, GPIO.IN)
 
 event = threading.Event()
 
-class serialThread (threading.Thread):
+lastDistance = 0
+
+threshold = config['dispenser']['threshold']
+
+class dispenserThread (threading.Thread):
     def __init__(self, threadID, name):
         threading.Thread.__init__(self)
         self.threadID = threadID
@@ -14,14 +32,63 @@ class serialThread (threading.Thread):
     def run(self):
         self.running = True
         print("Starting " + self.name)
-        readSerial(self)
+        runDispenser(self)
+        onExit(self)
         print("Exiting " + self.name)
     def stop(self):
         self.running = False
 
 # This connects to arduino via serial
-def readSerial(thread):
+def runDispenser(thread):
     from main import onDispense
     while thread.running:
-        event.wait(5)
-        onDispense()
+        x = etaisyys()
+        print(x)
+        if x <= threshold and lastDistance > threshold:
+            dispense()
+            onDispense()
+            event.wait(2)
+        lastDistance = x
+        event.wait(0.1)
+
+def onExit(thread):
+    pass
+
+def etaisyys():
+    # Aseta Trigger HIGH
+    GPIO.output(GPIO_TRIGGER, True)
+
+    # Aseta Trigger 0.01ms jalkeen LOW
+    time.sleep(0.00001)
+    GPIO.output(GPIO_TRIGGER, False)
+
+    aloitus_aika = time.time()
+    lopetus_aika = time.time()
+
+    # Tallenna aloitus_aika
+    while GPIO.input(GPIO_ECHO) == 0:
+        aloitus_aika = time.time()
+
+    # Tallenna lopetus_aika
+    while GPIO.input(GPIO_ECHO) == 1:
+        lopetus_aika = time.time()
+
+    # Aikaero
+    kulunut_aika = lopetus_aika - aloitus_aika
+    # Aanennopeus (34300 cm/s)
+    # jaetaan 2:lla, koska ultraääni menee edestakaisin
+    etaisyys = (kulunut_aika * 34300) / 2
+
+    return etaisyys
+
+def run(power, t):
+    if power>=0:
+        car.forward(power)
+    else:
+        car.backward(-power)
+    event.wait(t)
+    car.stop()
+
+def dispense():
+    run(100,2)
+    run(-100,1)
