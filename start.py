68828918be663511ac4c75a826716bef3a69987d
jusola <juulain@gmail.com>
2020-07-29 20:31:20 +0300

    Some modifications

--- start.py
+++ start.py
@@ -1,17 +1,16 @@
-from main import thread_detection#, thread_dispenser
+from main import thread_detection, thread_dispenser
 from display import runDisplay, quitDisplay
 import time
 import sys
 
 try:
     thread_detection.start()
-    #thread_dispenser.start()
+    thread_dispenser.start()
     while True: 
         runDisplay()
 except KeyboardInterrupt:
-    
     print("Keyboard interrupt")
     thread_detection.stop()
-#    thread_dispenser.stop()
+    thread_dispenser.stop()
     quitDisplay()
     sys.exit(0)
