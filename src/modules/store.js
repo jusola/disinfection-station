3574d6faeafe666089e950bea9df7e729c2846b9
jusola <juulain@gmail.com>
2020-07-29 16:42:44 +0300

    More quickfixes

--- src/modules/store.js
+++ src/modules/store.js
@@ -11,8 +11,8 @@ class Store {
     this.loadingCount += loadingChange
   }
 
-  getLoading = (loadingChange) => {
-    return loadingChange
+  getLoading = () => {
+    return this.loadingCount
   }
 
   setScores = (newScores) => {
